a6a9cf11f34c5069a8e619157bfe4ea0ccb0000e
zmmnvt8yxv-dev <zmmnvt8yxv@privaterelay.appleid.com>
2025-12-30 18:42:16 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "./",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/TatnallLegacy/" : "/",
   plugins: [react()],
 }));
+
