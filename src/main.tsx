bc089636fcb14dcc214f73387610454f72a870a1
zmmnvt8yxv-dev <zmmnvt8yxv@privaterelay.appleid.com>
2025-12-30 15:57:20 -0500

    Add season selection and wire data-driven views

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { App } from "./App";
 import "./styles/globals.css";
-import { dataLoader } from "./data/loader";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
@@ -14,15 +13,3 @@ root.render(
     </HashRouter>
   </React.StrictMode>
 );
-
-if (typeof window !== "undefined") {
-  requestAnimationFrame(() => {
-    void import("/app.js")
-      .then(({ initLegacyApp }) => {
-        initLegacyApp({ dataLoader });
-      })
-      .catch((error) => {
-        console.error("Unable to initialize legacy app.js module", error);
-      });
-  });
-}
