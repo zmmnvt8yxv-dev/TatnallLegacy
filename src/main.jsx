34b09ce1c4c8c6f2bb2585f3f2d6decf668631d8
zmmnvt8yxv-dev <zmmnvt8yxv@privaterelay.appleid.com>
2026-01-06 17:57:57 -0500

    Revert: remove Capacitor mobile scaffolding

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
+const routerBase = (import.meta.env.BASE_URL && import.meta.env.BASE_URL !== "./") ? import.meta.env.BASE_URL : "/";
 import App from "./App.jsx";
 import { DataProvider } from "./data/DataContext.jsx";
 import "./styles.css";
@@ -11,7 +13,7 @@ window.addEventListener("unhandledrejection", (e) => {
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL || "/"}>
+    <BrowserRouter basename={routerBase}>
       <DataProvider>
         <App />
       </DataProvider>
