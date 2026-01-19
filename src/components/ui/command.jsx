7e06a4b6e4a0be24370ce658190f51a64627386e
zmmnvt8yxv-dev <zmmnvt8yxv@privaterelay.appleid.com>
2026-01-18 22:08:07 -0500

    refactor: adjust import path for `cn` utility in the command component

--- src/components/ui/command.jsx
+++ src/components/ui/command.jsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Command as CommandPrimitive } from "cmdk"
 import { Search } from "lucide-react"
-import { cn } from "../lib/utils"
+import { cn } from "../../lib/utils"
 
 const Command = React.forwardRef(({ className, ...props }, ref) => (
     <CommandPrimitive
